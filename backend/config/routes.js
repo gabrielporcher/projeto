808fbe9d2297d3aa79f5a80f60998ce011994f61
gabrielporcher <gabrielporcher95@gmail.com>
2020-05-29 01:09:19 -0300

    funções de usuario

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,5 @@
 const admin = require('./permission')
+const self = require('./selfPermission')
 
 module.exports = app => {
     //Essas serão as unicas URLS publicas (Não precisam do token)
@@ -16,4 +17,10 @@ module.exports = app => {
         .put(admin(app.api.user.save))
         .get(admin(app.api.user.getById))
         .delete(admin(app.api.user.remove))
+
+    app.route('/user/:id') //Usuário editar seus dados
+        .all(app.config.passport.authenticate())
+        .put(self(app.api.user.save))
+        .get(self(app.api.user.getById))
+
 }
